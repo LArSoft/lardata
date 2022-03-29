b20e3d7935f07fdacf5e06de3b074b6939f86d9a
Lynn Garren <garren@fnal.gov>
2022-03-29 18:21:45 -0500

    separate implementation from service

---
+++ lardata/DetectorInfoServices/LArPropertiesServiceStandard.cc
@@ -0,0 +1,20 @@
+#include "lardata/DetectorInfoServices/LArPropertiesServiceStandard.h"
+
+
+//------------------------------------------------
+/// \todo these values should eventually come from a database
+//-----------------------------------------------
+detinfo::LArPropertiesServiceStandard::LArPropertiesServiceStandard(Parameters const& config,
+                                                                    art::ActivityRegistry& reg)
+  : fProp{config.get_PSet()}
+{
+  reg.sPreBeginRun.watch(this, &LArPropertiesServiceStandard::preBeginRun);
+}
+
+//----------------------------------------------
+void
+detinfo::LArPropertiesServiceStandard::preBeginRun(art::Run const& run)
+{
+  fProp.Update(run.run());
+}
+
--- lardata/DetectorInfoServices/LArPropertiesServiceStandard_service.cc
+++ lardata/DetectorInfoServices/LArPropertiesServiceStandard_service.cc
@@ -3,22 +3,5 @@
 #include "art/Framework/Services/Registry/ServiceDefinitionMacros.h"
 
 //------------------------------------------------
-/// \todo these values should eventually come from a database
-//-----------------------------------------------
-detinfo::LArPropertiesServiceStandard::LArPropertiesServiceStandard(Parameters const& config,
-                                                                    art::ActivityRegistry& reg)
-  : fProp{config.get_PSet()}
-{
-  reg.sPreBeginRun.watch(this, &LArPropertiesServiceStandard::preBeginRun);
-}
-
-//----------------------------------------------
-void
-detinfo::LArPropertiesServiceStandard::preBeginRun(art::Run const& run)
-{
-  fProp.Update(run.run());
-}
-
-//------------------------------------------------
 DEFINE_ART_SERVICE_INTERFACE_IMPL(detinfo::LArPropertiesServiceStandard,
                                   detinfo::LArPropertiesService)
